a321522c1e9e39eb2e2a3c82fd5044729d6ed34e
Jack <79340758+Hilton184@users.noreply.github.com>
2022-06-17 11:04:55 +0100

    Simplify shoppingbasket (#5)
    * Refactor code to have a much simpler structure.
    
    Define available products in data.py instead of importing from JSON file
    
    Remove products.py and promotions.py as do not need to represent these as objects.
    
    Refactor basket.py to reflect this simplified new structure
    
    Rewrite tests alongside this refactor
    
    * remove data JSON files as no longer required - now defined as data structures in the code
    
    * update documentation
    
    * add additional tests for basket.product_count
    
    * minor changes to tests
    
    * update poetry.lock
    
    * type improvements after running mypy static type checker
    
    * remove unrequired import

--- src/shoppingbasket/cli.py
+++ src/shoppingbasket/cli.py
@@ -1,54 +1,42 @@
 """Command line interface utility for the shoppingbasket module."""
 
 
+from typing import Iterable
+
 import click
 
 import shoppingbasket._utils
 from shoppingbasket.basket import Basket
-from shoppingbasket.catalog import Catalog, CatalogType
 
 
 def _handle_invalid_products_output(basket: Basket) -> None:
-    for product in basket.invalid_products:
+    for product in basket.invalid:
         print(
-            f"""Product "{product}" is an invalid product as it is not in the product data."""
+            f"""Product "{product}" is an invalid product. It has not been added to the basket."""
         )
-
-    for product in basket.unavailable_products:
-        print(f"""Product "{product}" is unavailable for purchase.""")
 
 
 def _handle_primary_output(basket: Basket) -> None:
     print(f"Subtotal: {shoppingbasket._utils._currency_format(basket.subtotal)}")
 
-    if not basket.promoted_products:
+    if not basket.total_discount:
         print("(No offers available)")
 
-    for product in basket.promoted_products:
-        print(product.promotion_message)
+    for promotion, discount in basket.promotion_discounts.items():
+
+        if discount:
+            print(f"{promotion}: -{shoppingbasket._utils._currency_format(discount)}")
 
     print(f"Total price: {shoppingbasket._utils._currency_format(basket.total)}")
 
 
 @click.command()
-@click.option(
-    "--products-catalog-filepath",
-    help="The relative filepath from the current working directory, to the JSON file dataset containing valid purchasable products and their attributes. Default handled in package internals to look in data/products.json, relative to the current working directory.",
-)
-@click.option(
-    "--promotions-catalog-filepath",
-    help="The relative filepath from the current working directory, to the JSON file dataset containing product promotions and their attributes. Default handled in package internals to look in data/promotions.json, relative to the current working directory.",
-)
 @click.argument("products", nargs=-1)
-def main(
-    products_catalog_filepath: str, promotions_catalog_filepath: str, products: str
-):
+def main(products: Iterable):
     """Entrypoint for running the command line utility of the shoppingbasket package. Specify one or more products (via the PRODUCTS positional argument) to add to the basket."""
     products = [*products]
 
-    products_catalog = Catalog(CatalogType.PRODUCTS, products_catalog_filepath)
-    promotions_catalog = Catalog(CatalogType.PROMOTIONS, promotions_catalog_filepath)
-    basket = Basket(products_catalog, promotions_catalog)
+    basket = Basket()
 
     for product_name in products:
         basket.add_product(product_name)
--- tests/test_shoppingbasket/test_cli.py
+++ tests/test_shoppingbasket/test_cli.py
@@ -8,21 +8,13 @@ from shoppingbasket.cli import main
 class Test_MainFunction:
     """Test CLI interface with shoppingbasket package."""
 
-    def test_single_available_product_no_promotion(
-        self, product_json_data_filepath: str, promotion_json_data_filepath: str
-    ):
+    def test_single_available_product_no_promotion(self):
         """Test single product."""
         products_list = ["MILK"]
 
         runner = CliRunner()
-        cli_args = [
-            "--products-catalog-filepath",
-            product_json_data_filepath,
-            "--promotions-catalog-filepath",
-            promotion_json_data_filepath,
-        ] + products_list
 
-        response = runner.invoke(main, cli_args)
+        response = runner.invoke(main, products_list)
 
         assert (
             response.output
@@ -30,76 +22,52 @@ class Test_MainFunction:
         )
         assert response.exit_code == 0
 
-    def test_single_available_product_on_promotion(
-        self, product_json_data_filepath: str, promotion_json_data_filepath: str
-    ):
-        """Test single product."""
+    def test_single_available_product_on_promotion(self):
+        """Test single product on promotion."""
         products_list = ["APPLES"]
 
         runner = CliRunner()
-        cli_args = [
-            "--products-catalog-filepath",
-            product_json_data_filepath,
-            "--promotions-catalog-filepath",
-            promotion_json_data_filepath,
-        ] + products_list
 
-        response = runner.invoke(main, cli_args)
+        response = runner.invoke(main, products_list)
 
         assert (
             response.output
-            == "Subtotal: £1.00\nApples 10% off: 10p\nTotal price: 90p\n"
+            == "Subtotal: £1.00\nApples 10% off: -10p\nTotal price: 90p\n"
         )
         assert response.exit_code == 0
 
-    def test_several_available_products(
-        self, product_json_data_filepath: str, promotion_json_data_filepath: str
-    ):
-        """Test single product."""
+    def test_several_available_products(self):
+        """Test several available products including promotions."""
         products_list = ["APPLES", "MILK", "SOUP", "BREAD"]
 
         runner = CliRunner()
-        cli_args = [
-            "--products-catalog-filepath",
-            product_json_data_filepath,
-            "--promotions-catalog-filepath",
-            promotion_json_data_filepath,
-        ] + products_list
 
-        response = runner.invoke(main, cli_args)
+        response = runner.invoke(main, products_list)
 
         assert (
             response.output
-            == "Subtotal: £3.75\nApples 10% off: 10p\nTotal price: £3.65\n"
+            == "Subtotal: £3.75\nApples 10% off: -10p\nTotal price: £3.65\n"
         )
         assert response.exit_code == 0
 
-    def test_several_available_products_with_soup_promo(
-        self, product_json_data_filepath: str, promotion_json_data_filepath: str
-    ):
-        """Test single product."""
+    def test_several_available_products_with_soup_promo(self):
+        """Test several available products including several promotions."""
         products_list = ["APPLES", "MILK", "SOUP", "BREAD", "SOUP"]
 
         runner = CliRunner()
-        cli_args = [
-            "--products-catalog-filepath",
-            product_json_data_filepath,
-            "--promotions-catalog-filepath",
-            promotion_json_data_filepath,
-        ] + products_list
 
-        response = runner.invoke(main, cli_args)
+        response = runner.invoke(main, products_list)
 
         assert (
             response.output
-            == "Subtotal: £4.40\nApples 10% off: 10p\nPurchase 2 tins of soup and get half price off bread: 40p\nTotal price: £3.90\n"
+            == "Subtotal: £4.40\nApples 10% off: -10p\nPurchase 2 tins of soup and get half price off bread: -40p\nTotal price: £3.90\n"
         )
         assert response.exit_code == 0
 
-    def test_several_available_products_with_soup_promo_and_invalid_products_and_unavailable_products(
-        self, product_json_data_filepath: str, promotion_json_data_filepath: str
+    def test_several_available_products_with_soup_promo_and_invalid_products(
+        self,
     ):
-        """Test single product."""
+        """Test several available products including several promotions including invalid products."""
         products_list = [
             "apples",
             "MILK",
@@ -113,29 +81,29 @@ class Test_MainFunction:
         ]
 
         runner = CliRunner()
-        cli_args = [
-            "--products-catalog-filepath",
-            product_json_data_filepath,
-            "--promotions-catalog-filepath",
-            promotion_json_data_filepath,
-        ] + products_list
 
-        response = runner.invoke(main, cli_args)
+        response = runner.invoke(main, products_list)
 
         assert response.exit_code == 0
 
-        assert """Product "TOMATOES" is unavailable for purchase.""" in response.output
         assert (
-            """Product "CHICKEN" is an invalid product as it is not in the product data."""
+            """Product "TOMATOES" is an invalid product. It has not been added to the basket."""
             in response.output
         )
-        assert """Product "cucumber" is unavailable for purchase.""" in response.output
         assert (
-            """Product "tEa" is an invalid product as it is not in the product data."""
+            """Product "CHICKEN" is an invalid product. It has not been added to the basket."""
+            in response.output
+        )
+        assert (
+            """Product "cucumber" is an invalid product. It has not been added to the basket."""
+            in response.output
+        )
+        assert (
+            """Product "tEa" is an invalid product. It has not been added to the basket."""
             in response.output
         )
 
         assert (
-            "Subtotal: £4.40\nApples 10% off: 10p\nPurchase 2 tins of soup and get half price off bread: 40p\nTotal price: £3.90\n"
+            "Subtotal: £4.40\nApples 10% off: -10p\nPurchase 2 tins of soup and get half price off bread: -40p\nTotal price: £3.90\n"
             in response.output
         )
